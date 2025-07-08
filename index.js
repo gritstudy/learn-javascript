152fd43dfaddf920bfdead589989a369f22d7c88
gritstudy <hrlee716@gmail.com>
2025-07-08 10:33:07 +0900

    class2day: js test

---
+++ index.js
@@ -0,0 +1,21 @@
+const cl = "multicampus";
+console.log("🚀 ~ cl:", cl);
+let comment = "good facility";
+
+console.log("cl", cl);
+
+comment = "bad air population";
+console.log("comment", typeof comment);
+
+comment = 1111;
+console.log("🚀 ~ comment:", comment);
+
+const bool = true;
+console.log("🚀 ~ bool:", bool);
+
+const bool1 = 0;
+console.log("🚀 ~ bool1:", typeof bool1);
+
+if (bool == true) {
+  console.log(" if 안에 :" + typeof bool);
+}
